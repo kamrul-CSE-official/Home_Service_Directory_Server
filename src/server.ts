bae4bb1a2bcc79cacd127dcb80709a48b261e6f1
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2023-07-05 22:50:24 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
-import { connect } from "mongoose";
+import { MongoClient, MongoClientOptions } from "mongodb";
 import app from "./app";
 import config from "./config";
 
-//Database connection
-const boostrap = async () => {
+// Database connection
+const bootstrap = async () => {
   try {
-    await connect(config.database_url as string);
-    console.log(`Database connection successful 😍`);
+    const options: MongoClientOptions = {
+      // Add any desired options here
+    };
+
+    const client = new MongoClient(config.database_url as string, options);
+
+    await client.connect();
+    console.log("Database connected!😍");
 
     app.listen(config.port, () => {
-      console.log(`This server is running ${config.port}`);
+      console.log(`This application is running on port ${config.port} 🏃`);
     });
+
+    client.close();
   } catch (err) {
-    console.log("Error to DB connection ", err);
+    console.log("Error connecting to the database:", err);
   }
 };
-boostrap();
+
+bootstrap();
