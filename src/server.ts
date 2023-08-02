0a6cab8236089c8e7a7349558e171cac86741974
MD.Kamrul Hasan ⚡️ <83531333+kamrul-CSE-official@users.noreply.github.com>
2023-08-02 22:48:23 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -66,7 +66,7 @@ async function run() {
 
     const usersCollection = client.db("home-service-directory").collection("users");
     const userMassages = client.db("home-service-directory").collection("massage");
-    const userState = client.db("home-service-directory").collection("state");
+    const userState = client.db("home-service-directory").collection("notification");
 
 
     //Validation with id
@@ -83,6 +83,71 @@ async function run() {
         return false;
       }
     }
+
+
+    //Set Message Notifation
+    async function massageState(senderId: string, receiverId: string) {
+      const query = { for: receiverId };
+      const existingDocument = await userState.findOne(query);
+
+      if (!existingDocument) {
+        // If there is no document for the receiverId, create a new one.
+        const newMessageNotification = {
+          for: receiverId,
+          data: [{ senderId: senderId, receiverId: receiverId, massageState: 1 }],
+        };
+        await userState.insertOne(newMessageNotification);
+      } else {
+        // If the document exists, check if there is a matching senderId and receiverId in the data array.
+        const dataMatchIndex = existingDocument.data.findIndex(
+          item => item.senderId === senderId && item.receiverId === receiverId
+        );
+
+        if (dataMatchIndex !== -1) {
+          // If there is a match, increment the state by 1 for the matching senderId and receiverId.
+          await userState.updateOne(
+            { _id: existingDocument._id, 'data.senderId': senderId, 'data.receiverId': receiverId },
+            { $inc: { 'data.$.massageState': 1 } }
+          );
+        } else {
+          // If there is no match, add a new entry to the data array with state = 1.
+          await userState.updateOne(
+            { _id: existingDocument._id },
+            { $push: { data: { senderId: senderId, receiverId: receiverId, massageState: 1 } } }
+          );
+        }
+      }
+    }
+    //Get Message Notifation
+    app.get('/massageNotifationGet', async (req: Request, res: Response) => {
+      async function sumMessageStates(receiverId: string): Promise<number> {
+        const query = { for: receiverId };
+        const document = await userState.findOne(query);
+
+        if (!document) {
+          return 0; // Return 0 if the document doesn't exist
+        }
+
+        // Use MongoDB aggregation pipeline to sum the massageState values in the data array
+        const sumResult = await userState.aggregate([
+          { $match: { for: receiverId } },
+          { $unwind: '$data' },
+          { $group: { _id: '$_id', totalState: { $sum: '$data.massageState' } } }
+        ]).toArray();
+
+        if (sumResult.length === 0) {
+          return 0; // Return 0 if the data array is empty
+        }
+
+        return sumResult[0].totalState;
+      };
+      const { id } = req.query;
+      if (await isValid(id as string)) {
+        const totalState = await sumMessageStates(id as string);
+        res.status(200).json(totalState);
+      };
+    });
+
 
 
 
@@ -217,49 +282,59 @@ async function run() {
     });
 
 
-    //chat
-    app.post('/chat', async (req, res) => {
+
+
+
+
+
+
+
+    //chat post
+    app.post('/chat', async (req: Request, res: Response) => {
       try {
         const { sender, recever, message, time, date } = req.body;
-        const chat = {
-          message: message,
-          time: time,
-          date: date,
-          send: sender
+        if (await isValid(sender) && await isValid(recever)) {
+          const chat = {
+            message: message,
+            time: time,
+            date: date,
+            send: sender
+          }
+          // Assuming 'userMassages' is your MongoDB collection
+          const query = { sender, recever };
+          const existingDocument = await userMassages.findOne(query);
+
+          if (!existingDocument) {
+            // If conversation doesn't exist, create a new one with the first message
+            const newConversation = {
+              sender,
+              recever,
+              message: [chat],
+            };
+            // Insert the new conversation into the collection
+            await userMassages.insertOne(newConversation);
+            massageState(sender, recever);
+
+          } else {
+            // If conversation already exists, push the new message to the 'message' array
+            existingDocument.message.push(chat);
+
+            // Update the document with the new message
+            await userMassages.updateOne(query, { $set: { message: existingDocument.message } });
+            massageState(sender, recever);
+          }
+          res.status(200).json({ message: 'New message added successfully' });
         }
 
-        // Assuming 'userMassages' is your MongoDB collection
-        const query = { sender, recever };
-        const existingDocument = await userMassages.findOne(query);
-
-        if (!existingDocument) {
-          // If conversation doesn't exist, create a new one with the first message
-          const newConversation = {
-            sender,
-            recever,
-            message: [chat],
-          };
-          // Insert the new conversation into the collection
-          await userMassages.insertOne(newConversation);
-        } else {
-          // If conversation already exists, push the new message to the 'message' array
-          existingDocument.message.push(chat);
-
-          // Update the document with the new message
-          await userMassages.updateOne(query, { $set: { message: existingDocument.message } });
-        }
-
-        res.status(200).json({ message: 'New message added successfully' });
       } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'An error occurred' });
       }
     });
-
-    app.get('/chat', async (req, res) => {
+    //Get single chat throw id's
+    app.get('/chat', async (req: Request, res: Response) => {
       try {
         const { sender, recever } = req.query;
-
         // Find chat messages where both sender and receiver match the provided IDs
         const query = {
           $or: [
@@ -275,6 +350,92 @@ async function run() {
         res.status(500).json({ message: 'An error occurred' });
       }
     });
+
+
+    //get user name, id, img
+    async function Connector(id: string) {
+      try {
+        // const { id } = req.query;
+
+        if (await isValid(id as string)) {
+          const query = { _id: new ObjectId(id as string) };
+
+          // Use generic version of findOne to specify the return type explicitly
+          const userInfo = await usersDetails.findOne<{ firstName: string; lastName: string; email: string; photoURL: string, id: string }>(query);
+
+          if (userInfo) {
+            const user = { name: `${userInfo.firstName} ${userInfo.lastName}`, photoURL: userInfo.photoURL, id: id };
+            return user;
+            // res.json(user);
+          } else {
+            // res.status(404).send('User not found');
+          }
+        } else {
+          // res.status(404).send('Invalid user ID');
+        }
+      } catch (err) {
+        console.log('Error: ', err);
+        // res.status(500).send('Internal server error');
+      }
+
+    }
+
+
+    //Get all chat throw users id's
+    app.get('/allConnectors', async (req: Request, res: Response) => {
+      try {
+        const { id } = req.query;
+        const userID = id as string;
+
+        if (await isValid(userID)) {
+          const query = { $or: [{ sender: userID }, { receiver: userID }] };
+          const userChatMessages = await userMassages.find(query).toArray();
+
+          let info: {
+            name: string;
+            photoURL: string;
+            id: string;
+          }[] = [];
+
+          for (let i = 0; i < userChatMessages.length; i++) {
+            if (userID !== userChatMessages[i]?.sender) {
+              const data: {
+                name: string;
+                photoURL: string;
+                id: string;
+              } | undefined = await Connector(userChatMessages[i]?.sender);
+
+              if (data !== undefined) {
+                console.log(data);
+                info.push(data);
+              }
+            }
+
+            if (userID !== userChatMessages[i]?.recever) {
+              const data: {
+                name: string;
+                photoURL: string;
+                id: string;
+              } | undefined = await Connector(userChatMessages[i]?.recever);
+
+              if (data !== undefined) {
+                info.push(data);
+              }
+            }
+          }
+          res.send(info);
+        } else {
+          res.status(404).send('User not found...');
+        }
+      } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).send('Internal Server Error');
+      } finally {
+
+        // client.close();
+      }
+    });
+
 
 
 
